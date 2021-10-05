c32466f56d83d12dd721689de92961b9334e1e6e
“iwasawa <iwasi_44@hotmail.com>
2021-10-05 15:09:42 +0900

    新規投稿、投稿一覧機能　実装

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,25 @@
+class PicturesController < ApplicationController
+  def index
+    @pictures = Picture.all
+  end
+  def new
+    @picture = Picture.new
+  end
+  def create
+    @picture = Picture.new(picture_params)
+    @picture.user_id = current_user.id
+    if @picture.save
+      redirect_to pictures_path, notice: "投稿しました"
+    else
+      render :new
+    end
+  end
+  private
+  def picture_params
+    params.require(:picture).permit(:title, :content, :content_picture)
+  end
+  
+  
+  
+  
+end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,4 @@
 class Picture < ApplicationRecord
+  mount_uploader :content_picture, ContentPictureUploader
   belongs_to :user
 end
