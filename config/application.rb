84f11d3800ce4b1aadb275052454afd0aa573afa
“iwasawa <iwasi_44@hotmail.com>
2021-10-07 15:17:14 +0900

    final

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module Instapic
       g.assets false
       g.helper false
     end
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
